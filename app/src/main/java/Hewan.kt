3c24423209a354ce19a53bba7083390e31932a85
keysarayodya <keysarayodya@gmail.com>
2021-05-05 02:06:39 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/Hewan.kt
+++ app/src/main/java/Hewan.kt
@@ -3,5 +3,6 @@ import org.d3if0044.galerihewan.R
 data class Hewan (
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int = R.drawable.angsa
+    val imageId: String
+//    val jenis: String
 )
