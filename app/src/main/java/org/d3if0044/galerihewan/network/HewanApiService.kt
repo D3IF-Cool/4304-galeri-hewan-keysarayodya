372f12e012a147aaf418e2b4bb8fc200d017d97e
keysarayodya <keysarayodya@gmail.com>
2021-05-05 02:39:22 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if0044/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/org/d3if0044/galerihewan/network/HewanApiService.kt
@@ -1,19 +1,25 @@
 package org.d3if0044.galerihewan.network
 
+import Hewan
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private const val BASE_URL = "https://dif.indraazimi.com/"
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface HewanApiService {
     @GET("listhewan.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {
